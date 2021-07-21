6235ca2b7e1a0ec798466e08140275929760bd12
canxiangzhao <18813035114@163.com>
2021-07-22 01:15:11 +0800

    add binary search

--- dataStructure/Tree/binaryTree.js
+++ dataStructure/Tree/binaryTree.js
@@ -7,6 +7,9 @@
  * }
  */
 
+// 递归相对简单，迭代一定要记得用Stack，围绕先进后出的顺序进行考虑迭代。
+// 可以从一个简单的二叉树进行归纳。
+
  root = {
   val: 1,
   left: {
@@ -93,25 +96,72 @@ function inorderTraversal(root) {
 }
 
 /**
- * 前序遍历-迭代
+ * 中序遍历-迭代
  * 根(前)-左-右
- * 特殊case：[] => [] 
+ * 特殊case：[] => []
+ * 仿照递归写法
  * @param {TreeNode} root 
  * @returns 
  */
  function inorderTraversal(root) {
-  if (!root) return;
   let result = [];
-  let stack = [root];
-  while(stack.length) {
-    node = stack.pop();
-    if (node.right) stack.push(node.right);
-    if (node.left) status.push(node.left)
-                                                                                                                                                                                                                                                                         
+  let stack = [];
+  while(stack.length || root) {
+    if (root) {
+      stack.push(root.val);
+      root = root.left;
+    } else {
+      root = stack.pop();
+      result.push(root.val);
+      root = root.right;
+    }                                                                                                                                                                                                                                                                    
   }
   return result;
  }
 
 
-// console.log('pre', preOrderTraversal(root));
+ //======后序遍历=====
+ /**
+  * 后序遍历-递归
+  * @param {TreeNode} root 
+  * @returns 
+  */
+ var postorderTraversal = function(root) {
+  let result = [];
+
+  let traversal = (node) => {
+      if (node) {
+          traversal(node.left);
+          traversal(node.right);
+          result.push(node.val)
+      }
+  }
+  traversal(root);
+  return result;
+
+};
+
+//======后序遍历=====
+ /**
+  * 后序遍历-迭代
+  * 将前序遍历变体根-右-左进行reverse => 左-右-根 
+  * @param {TreeNode} root 
+  * @returns 
+  */
+  var postorderTraversal = function(root) {
+    if (!root) return []; // 边界条件
+    let result = [];
+    let stack = [root];
+    while (stack.length) {
+        node = stack.pop(); // 从队列弹出一个node;
+        result.push(node.val);
+        if (node.left) stack.push(node.left);
+        if (node.right) stack.push(node.right);
+    }
+    return result.reverse()
+  };
+
+
+
+console.log('pre', postorderTraversal(root));
 
